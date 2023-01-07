84737cad98d49dbf2acb1c759ecde33efd11ef43
tokuno-masahiko <tokuno5956@gmail.com>
2023-01-07 21:01:50 +0900

    出品　完成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -23,32 +23,32 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'category_idが空では出品できない' do
-        @item.category_id = ''
+        @item.category_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'status_idが空では出品できない' do
-        @item.status_id = ''
+        @item.status_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it 'fee_idが空では出品できない' do
-        @item.fee_id = ''
+        @item.fee_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Fee can't be blank")
       end
       it 'prefecture_idが空では出品できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'shipping_date_idが空では出品できない' do
-        @item.shipping_date_id = ''
+        @item.shipping_date_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping date can't be blank")
       end
       it 'item_feeが空では出品できない' do
-        @item.item_fee = ''
+        @item.item_fee = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Item fee can't be blank")
       end
