d6faec867cf3f6072ae5de75cbf91d44a83ec721
tokuno-masahiko <tokuno5956@gmail.com>
2023-01-08 17:08:41 +0900

    商品詳細機能　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,24 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
