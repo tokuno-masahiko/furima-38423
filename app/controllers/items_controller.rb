eddc21f009626940867cfa60b84f70c01cf20bad
tokuno-masahiko <tokuno5956@gmail.com>
2023-01-09 21:27:15 +0900

    削除機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_tweet, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: :edit
 
@@ -21,9 +21,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
+    if current_user.id == @item.user_id 
+     @item.destroy
     end
+    redirect_to root_path
   end
 
   def edit
