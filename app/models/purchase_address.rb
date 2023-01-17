733c1ef6988ad0a076676b1026af9ee71353ef21
tokuno-masahiko <tokuno5956@gmail.com>
2023-01-17 14:54:06 +0900

    Update purchase_address.rb

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,8 +3,8 @@ class PurchaseAddress
   attr_accessor :postal_code, :prefecture_id, :city, :address, :house_number, :phone_number, :user_id, :item_id, :token, :purchase_id
   
   with_options presence: true do
-    validates :phone_number, format: {with: /\A0\d{9,10}\z/, message: 'is invalid'}
-    validates :user_id, :city, :address, :prefecture_id, :token
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: 'is invalid'}
+    validates :user_id, :city, :address, :prefecture_id, :token, :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     # validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   end
